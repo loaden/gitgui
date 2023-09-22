f3315457c68371d473a10d9be5057191c0a27b28
loaden <loaden@live.com>
2023-09-22 08:08:37 +0800

    Add core of native library

---
+++ crates/core/src/lib.rs
@@ -0,0 +1,17 @@
+pub const G_APPNAME: &str = "GitGui";
+pub const G_VERSION: &str = "0.0.1";
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
