379ed2e37db93dfc45f119ab2b4c5e1ee3f03889
loaden <loaden@live.com>
2023-09-24 12:49:35 +0800

    Integration with local Corrosion cmake module

--- windows/rust.cmake
+++ windows/rust.cmake
@@ -2,19 +2,22 @@
 # many dependencies we would need to install Corrosion on the system.
 # See instructions on https://github.com/corrosion-rs/corrosion#cmake-install
 # Once done, uncomment this line:
-# find_package(Corrosion REQUIRED)
+set(Corrosion_DIR ${CMAKE_CURRENT_SOURCE_DIR}/cmake/lib/cmake/Corrosion)
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
+find_package(Corrosion REQUIRED)
 
-include(FetchContent)
+# include(FetchContent)
 
-FetchContent_Declare(
-    Corrosion
-    GIT_REPOSITORY git@github.com:corrosion-rs/corrosion.git
-    GIT_TAG origin/master # Optionally specify a version tag or branch here
-)
+# FetchContent_Declare(
+#     Corrosion
+#     GIT_REPOSITORY git@github.com:corrosion-rs/corrosion.git
+#     GIT_TAG v0.4.3 # Optionally specify a version tag or branch here
+# )
 
-FetchContent_MakeAvailable(Corrosion)
+# FetchContent_MakeAvailable(Corrosion)
 
 corrosion_import_crate(MANIFEST_PATH ../native/Cargo.toml IMPORTED_CRATES imported_crates)
+
 target_link_libraries(${BINARY_NAME} PRIVATE ${imported_crates})
 foreach(imported_crate ${imported_crates})
   list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${imported_crate}-shared>)
