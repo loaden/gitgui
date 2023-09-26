c7816821f258a521603af73bb3e2f85df31cf678
loaden <loaden@live.com>
2023-09-26 23:16:11 +0800

    Simplest  StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: Text(
+void main() => runApp(const Home());
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
       'Hello World!',
       textDirection: TextDirection.ltr,
-    ),
-  ));
+    );
+  }
 }
