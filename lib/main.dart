6f519c2c16007ca880258087fa79b88238cce6ff
loaden <loaden@live.com>
2023-09-26 23:02:57 +0800

    Simplest MaterialApp application

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      const Text(
-        'Hello World!',
-        textDirection: TextDirection.ltr,
-      ),
-    );
+void main() {
+  runApp(const MaterialApp(
+    home: Text(
+      'Hello World!',
+      textDirection: TextDirection.ltr,
+    ),
+  ));
+}
