d39d7c0c975103002a3b6d37fa472addef9c55ab
loaden <loaden@live.com>
2023-09-26 22:57:09 +0800

    Hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'global.dart';
-import 'native.dart';
-
-void main() => runApp(const App());
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: G_APPNAME,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 219, 85, 85)),
-        useMaterial3: true,
-      ),
-      home: const HomePage(title: '这是一个测试页面'),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-  void _incrementCounter() => setState(() => _counter++);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            FutureBuilder<List<dynamic>>(
-                future: Future.wait([api.helloFromRust(count: 3)]),
-                builder: (context, snap) {
-                  final data = snap.data;
-                  if (data == null) return const Text("Loading");
-                  return Text('${data[0]}');
-                }),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: '增加',
-        child: const Icon(Icons.add),
+void main() => runApp(
+      const Text(
+        'Hello World!',
+        textDirection: TextDirection.ltr,
       ),
     );
-  }
-}
