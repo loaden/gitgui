999fe5f7f0454b4d9044c0c47a20ec2c101d1456
loaden <loaden@live.com>
2023-09-27 19:14:55 +0800

    Implement the stateful button

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,42 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Hi')),
-      body: ElevatedButton(
-        onPressed: _add,
-        style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
-        child: const Icon(Icons.add),
+      body: const Row(
+        children: [
+          Button(),
+          Button(),
+        ],
       ),
+    );
+  }
+}
+
+class Button extends StatefulWidget {
+  const Button({super.key});
+
+  @override
+  State<Button> createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
+  bool _chcl = false;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: _add,
+      onLongPress: _changeColor,
+      style: ElevatedButton.styleFrom(
+          backgroundColor: _chcl ? Colors.orange : Colors.green),
+      child: const Icon(Icons.add),
     );
   }
 
   // ignore: avoid_print
   void _add() => print('hello');
+
+  void _changeColor() {
+    setState(() {
+      _chcl = !_chcl;
+    });
+  }
 }
