7211425ac1c98991b5a0c2dca389dd23e21a0fc2
loaden <loaden@live.com>
2023-09-27 19:42:35 +0800

    Send event to parent

--- lib/main.dart
+++ lib/main.dart
@@ -16,18 +16,31 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Hi')),
-      body: const Row(
+      body: Row(
         children: [
-          Button(),
-          Button(),
+          Button(
+            onPressed: _onBtn1,
+          ),
+          Button(
+            onPressed: _onBtn2,
+          ),
         ],
       ),
     );
   }
+
+  void _onBtn1(bool b) {
+    print('1 $b');
+  }
+
+  void _onBtn2(bool b) {
+    print('2 $b');
+  }
 }
 
 class Button extends StatefulWidget {
-  const Button({super.key});
+  const Button({super.key, required this.onPressed});
+  final ValueChanged<bool> onPressed;
 
   @override
   State<Button> createState() => _ButtonState();
@@ -38,7 +51,7 @@ class _ButtonState extends State<Button> {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: _add,
+      onPressed: _press,
       onLongPress: _changeColor,
       style: ElevatedButton.styleFrom(
           backgroundColor: _chcl ? Colors.orange : Colors.green),
@@ -46,8 +59,7 @@ class _ButtonState extends State<Button> {
     );
   }
 
-  // ignore: avoid_print
-  void _add() => print('hello');
+  void _press() => widget.onPressed(_chcl);
 
   void _changeColor() {
     setState(() {
