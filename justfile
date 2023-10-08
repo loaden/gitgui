757e3d71b55c7fe20f506d3a2bdb59ba8713e4c0
loaden <loaden@live.com>
2023-10-08 10:05:52 +0800

    Optimized CMake configuration

--- justfile
+++ justfile
@@ -1,11 +1,20 @@
 set windows-shell := ["powershell.exe", "-NoLogo", "-Command"]
 llvm_path := if os_family() == "windows" { "--llvm-path \"" + replace(trim(`where.exe clang`), "\\bin\\clang.exe", "\"") } else { "" }
 c_path := if os() == "linux" { 'CPATH="$(clang -v 2>&1 | grep "Selected GCC installation" | rev | cut -d" " -f1 | rev)/include"' } else { "" }
-
-export CARGO_BUILD_TYPE := "Debug"
+target := if os() == "macos" { "macos" } else if os() == "windows" { "windows" } else { "linux" }
+export BUILD_TYPE := if os_family() == "windows" { "Debug" } else { "" }
 
 run:
-  flutter run
+  flutter run -d {{target}}
+
+build $BUILD_TYPE="Release":
+  flutter build {{target}}
+
+debug:
+  echo ${BUILD_TYPE}
+
+release $BUILD_TYPE="Release":
+  echo ${BUILD_TYPE}
 
 gen:
   {{c_path}} flutter_rust_bridge_codegen {{llvm_path}} \
@@ -17,10 +26,6 @@ gen:
     --class-name Native \
     --dart-format-line-length 80 \
     --inline-rust
-
-build:
-  cargo build --manifest-path native/Cargo.toml
-  dart run build_runner build
 
 get:
   flutter pub get
--- rust.cmake
+++ rust.cmake
@@ -1,7 +1,7 @@
 set(ENV{CARGO_TARGET_DIR})
 message(STATUS $ENV{CARGO_TARGET_DIR})
 
-if(CMAKE_BUILD_TYPE STREQUAL "Debug" OR $ENV{CARGO_BUILD_TYPE} STREQUAL "Debug")
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
   set(CARGO_BUILD_TYPE "")
   set(TARGET_DIR "debug")
 else()
@@ -9,19 +9,25 @@ else()
   set(TARGET_DIR "release")
 endif()
 
-set(CARGO_MANIFEST_PATH "--manifest-path=../native/Cargo.toml")
+if("$ENV{BUILD_TYPE}" STREQUAL "Debug")
+  set(CARGO_BUILD_TYPE "")
+  set(TARGET_DIR "debug")
+endif()
 
 if(CMAKE_HOST_SYSTEM_NAME MATCHES "Linux")
-	set(NATIVE_LIBRARY "../target/${TARGET_DIR}/libnative.so")
+  set(NATIVE_LIBRARY "../target/${TARGET_DIR}/libnative.so")
 elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Windows")
-	set(NATIVE_LIBRARY "../target/${TARGET_DIR}/native.dll")
+  set(NATIVE_LIBRARY "../target/${TARGET_DIR}/native.dll")
 else()
   set(NATIVE_LIBRARY "non-exist")
 endif()
 
+set(CARGO_MANIFEST_PATH "--manifest-path=../native/Cargo.toml")
+
 message(CMAKE_BUILD_TYPE " = " ${CMAKE_BUILD_TYPE})
-message(CARGO_BUILD_TYPE " = " $ENV{CARGO_BUILD_TYPE})
+message(BUILD_TYPE " = " $ENV{BUILD_TYPE})
 message(NATIVE_LIBRARY " = " ${NATIVE_LIBRARY})
+message(TARGET_DIR " = " ${TARGET_DIR})
 
 add_custom_target(native ALL
   COMMAND cargo build ${CARGO_BUILD_TYPE} ${CARGO_MANIFEST_PATH}
