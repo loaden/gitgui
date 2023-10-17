fb71daecde80922da2f2911562da549fda589904
loaden <loaden@live.com>
2023-10-17 17:16:49 +0800

    Try ConstrainedBox layout

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -23,24 +23,64 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(rustHiText ?? '')),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Button(onPressed: (b) => _onBtn(b, context)),
-            const SizedBox(width: 10),
-            Button(onPressed: (b) async {
-              var r = await Navigator.of(context).pushNamed(route.aboutPage);
-              print(r);
-            }),
-          ],
-        ),
+      body: Row(
+        children: [
+          ConstrainedBox(
+            constraints: const BoxConstraints(
+              minHeight: 50,
+              minWidth: 150,
+              maxHeight: double.infinity,
+              maxWidth: 150,
+            ),
+            child: const Column(
+              children: [
+                TextField(),
+                TextField(),
+                TextField(),
+                TextField(),
+                TextField(),
+              ],
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            children: [
+              ConstrainedBox(
+                constraints: const BoxConstraints(
+                  minHeight: 60,
+                  minWidth: 200,
+                  maxHeight: double.infinity,
+                  maxWidth: 300,
+                ),
+                child: const TextField(),
+              ),
+              const SizedBox(
+                width: 180,
+                child: TextField(),
+              ),
+              const SizedBox(height: 20),
+              navRoute(context),
+            ],
+          )
+        ],
       ),
     );
   }
 
-  void _onBtn(bool b, BuildContext context) {
-    Navigator.of(context).pushNamed(route.configPage);
+  Row navRoute(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Button(
+          onPressed: (b) => Navigator.of(context).pushNamed(route.configPage),
+        ),
+        const SizedBox(width: 10),
+        Button(onPressed: (b) async {
+          var r = await Navigator.of(context).pushNamed(route.aboutPage);
+          print(r);
+        }),
+      ],
+    );
   }
 
   Future<void> _callHelloFromRust() async {
